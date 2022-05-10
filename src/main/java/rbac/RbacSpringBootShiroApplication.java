4cf4b251c6b99706512d3c8311eeba26e43e82c1
wangsc801 <wangsc801@163.com>
2022-05-10 21:58:57 +0800

    first commit.

---
+++ src/main/java/rbac/RbacSpringBootShiroApplication.java
@@ -0,0 +1,13 @@
+package rbac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RbacSpringBootShiroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RbacSpringBootShiroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/rbac/RbacSpringBootShiroApplicationTests.java
@@ -0,0 +1,13 @@
+package rbac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RbacSpringBootShiroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
